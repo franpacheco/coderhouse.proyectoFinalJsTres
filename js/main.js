035fe7a04fd2420d0e8ab3d7ec5a59555010cf2f
Fran Pacheco <pachecofrancisco1999@gmail.com>
2024-04-26 18:04:11 -0300

    Se realizo modificacion en css para hacer el sitio responsive - Se agregaron librerias - Se utilizo el fetch para cargar los productos en la web por medio de un archivo .json

--- js/main.js
+++ js/main.js
@@ -1,37 +1,46 @@
 const portfolio = JSON.parse(localStorage.getItem("portfolio")) || [];
 
-const cursos = [
-    {
-        id:"inicial",
-        titulo:"Curso de inicial en uñas",
-        precio:10000,
-        img:"./img/INICIAL.png",   
-    },
-    {   
-        id:"polygel",
-        titulo:"Curso de polygel en uñas",
-        precio:15000,
-        img:"./img/POLYGEL.png",   
-    },
-    {
-        id:"kapping",
-        titulo:"Curso de kapping en uñas",
-        precio:20000,
-        img:"./img/KAPPING.png",   
-    },    
-    {
-        id:"manicuria rusa",
-        titulo:"Curso de Manicuria Rusa en uñas",
-        precio:5000,
-        img:"./img/MANICURIA-RUSA.png",   
-    }
-];
-
 const contenedorCursos = document.querySelector("#cursos");
 const portfolioVacio = document.querySelector("#portfolio-vacio");
 const portfolioCursos = document.querySelector("#portfolio-cursos");
 const portfolioTotal = document.querySelector("#portfolio-total");
 const checkoutBtn = document.querySelector("#checkout-btn");
+const vaciarBtn = document.querySelector("#vaciar-btn");
+
+let cursos = [];
+
+fetch("./data/cursos.json")
+    .then((res) => res.json())
+    .then((data) => {
+        cursos = [...data];
+        mostrarCursos(cursos); // Llama a la función mostrarCursos() después de obtener los datos
+    })
+    .catch((error) => {
+        console.error("Error al cargar los cursos:", error);
+    });
+
+// Función para mostrar los cursos en la página
+const mostrarCursos = (cursos) => {
+    cursos.forEach((curso) => {
+        let div = document.createElement("div");
+        div.classList.add("curso");
+        div.innerHTML = `
+            <img class="curso-img" src="${curso.img}">
+            <h3>${curso.titulo}</h3>
+            <p>$${curso.precio}</p>
+        `;
+
+        let button = document.createElement("button");
+        button.classList.add("curso-btn");
+        button.innerText = "Agregar al portfolio";
+        button.addEventListener("click", () => {
+            agregarAlPortfolio(curso);
+        });
+
+        div.append(button);
+        contenedorCursos.append(div);
+    });
+};
 
 const incrementarCantidad = (id) => {
     const curso = portfolio.find(item => item.id === id);
@@ -114,6 +123,17 @@ const agregarAlPortfolio = (curso) => {
         portfolio.push( {...curso, cantidad: 1} );    
     }   
     actualizarPortfolio(); 
+
+    Toastify({
+        text: "producto agregado al carrito",
+        duration: 3000,
+        newWindow: true,
+        style: {
+          background: "linear-gradient(to right, #ff00f7, #f86cf3)",
+          borderRadius: ".5rem"
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
 }
 
 const borrarDelPortfolio = (curso) => {
@@ -147,3 +167,31 @@ document.getElementById("checkout-form").addEventListener("submit", (event) => {
 });
 
 actualizarPortfolio();
+
+vaciarBtn.addEventListener("click", () => {
+
+    Swal.fire({
+        title: "¿vaciar portfolio?",
+        text: "se perderan todos los productos cargados hasta el momento",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, vaciar portfolio",
+        cancelButtonText: "No, Cancelar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Portfolio vacio",
+            icon: "success"
+          });
+        }
+      });
+
+    // Vaciar el carrito (eliminar todos los elementos del arreglo)
+    portfolio.splice(0, portfolio.length);
+
+    // Actualizar la interfaz para mostrar que el carrito está vacío
+    actualizarPortfolio();
+});
+
